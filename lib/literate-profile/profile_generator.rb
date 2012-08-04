dc8c7786c9dbbdda4e27deb9a272629ddc69e85e
Lee Dohm <lee@liftedstudios.com>
2012-08-04 12:59:02 -0700

    Making progress with the ProfileGenerator.

--- lib/literate-profile/profile_generator.rb
+++ lib/literate-profile/profile_generator.rb
@@ -11,14 +11,28 @@ module Literate
       # Initializes a new instance of the +ProfileGenerator+ class.
       def initialize
         @path = nil
+        @shebang = nil
+      end
+
+      # Dumps the profile.
+      def dump
+        puts "#!#{@shebang}"
       end
 
       # Gets or sets the path to which to write the profile.
       # 
-      # @param path New value to set the path to or +nil+.
+      # @param path New value to set the path to, if any.
       def path(path = nil)
         @path = path unless path.nil?
         @path
+      end
+
+      # Gets or sets the shebang to place at the top of the profile, if any.
+      # 
+      # @param shebang New value to set for the shebang, if any.
+      def shebang(shebang = nil)
+        @shebang = shebang unless shebang.nil?
+        @shebang
       end
     end
   end
--- spec/profile_generator_spec.rb
+++ spec/profile_generator_spec.rb
@@ -14,10 +14,11 @@ require 'literate-profile'
 include Literate::Profile
 
 describe ProfileGenerator do
-  it 'will have no path when instantiated' do
+  it 'will have no path or shebang when instantiated' do
     generator = ProfileGenerator.new
 
     generator.path.must_be_nil
+    generator.shebang.must_be_nil
   end
 
   it 'can have its path set' do
@@ -26,5 +27,22 @@ describe ProfileGenerator do
     generator.path 'foo'
 
     generator.path.must_equal 'foo'
+  end
+
+  it 'can have its shebang set' do
+    generator = ProfileGenerator.new
+
+    generator.shebang 'foo'
+
+    generator.shebang.must_equal 'foo'
+  end
+
+  it 'will output to STDOUT if no path is set' do
+    generator = ProfileGenerator.new
+    generator.shebang '/bin/zsh'
+
+    proc {
+      generator.dump
+    }.must_output "#!/bin/zsh\n"
   end
 end
